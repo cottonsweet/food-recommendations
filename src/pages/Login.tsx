0975bfc0ef4bcf80d6f80ffd67c23e9ef219e660
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-18 23:29:20 +0900

    Login Route 퍼블리싱 작업

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import backGroundImg from "../assets/backGround.jpg";
+import styles from "../styles/pages/Login.module.css";
+
 const Login = () => {
-  return <div>로그인 페이지</div>;
+  const path = useNavigate();
+  const handleCancleBtn = () => path("/");
+  return (
+    <div className={styles.login_wrap}>
+      <div className={styles.login_img_box}>
+        <img className={styles.login_img} src={backGroundImg} />
+      </div>
+      <div className={styles.login_title}>
+        <div>오늘,</div>
+        <span>뭘</span>
+        <span>먹을까?</span>
+      </div>
+      <div className={styles.login_section}>
+        <div className={styles.login_section_wrap}>
+          <div onClick={handleCancleBtn} className={styles.login_cancle__btn}>
+            돌아가기
+          </div>
+          <div className={styles.login_section__login}>로그인</div>
+        </div>
+        <div className={styles.login_user_wrap}>
+          <form className={styles.login_form}>
+            <input className={styles.loign_email} placeholder="이메일 주소" />
+            <input className={styles.login_password} placeholder="비밀번호" />
+            <button className={styles.login_btn}>로그인</button>
+          </form>
+        </div>
+        <div className={styles.login_create_wrap}>
+          <span className={styles.login_create_btn}>가입하신 계정이 없으신가요 ?</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
