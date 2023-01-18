1ae7bc8832091f4f6380e886d93a48edbcc831e4
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-19 00:16:07 +0900

    Main 컴포넌트 생성 및, firebase Login state 변경함수 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { auth } from "./Auth";
+import { onAuthStateChanged } from "firebase/auth";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
+import Main from "./components/Main";
 
 function App() {
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => (user ? setLogin(true) : setLogin(false)));
+  });
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-      </Routes>
+      {login ? (
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+        </Routes>
+      )}
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <div>여기 메인 컴포넌트임</div>;
+};
+
+export default Main;
