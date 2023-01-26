2613b1978d02139e10604937720cfcdd5cb46a0b
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-26 23:58:29 +0900

    ResultModal 주석내용 추가

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -1,7 +1,14 @@
-type Good = {
-  result: string;
-};
+// Type 키워드
+// type Good = {
+//   result: string;
+// };
 
+// Interface 키워드
+interface Good {
+  result: string;
+}
+
+// props는 왜 Type or Interface 키워드로 지정을 해줘야하는가 ?
 const ResultModal = (props: Good) => {
   return <div>{props.result}</div>;
 };
