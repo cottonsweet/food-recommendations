f79d3e35354a38b7d66b0c2e636624f424513942
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-27 22:48:51 +0900

    ResultModal 컴포넌트 퍼블리싱

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -1,3 +1,5 @@
+import styles from "../styles/components/ResultModal.module.css";
+
 // Type 키워드
 // type Good = {
 //   result: string;
@@ -10,7 +12,14 @@ interface Good {
 
 // props는 왜 Type or Interface 키워드로 지정을 해줘야하는가 ?
 const ResultModal = (props: Good) => {
-  return <div>{props.result}</div>;
+  return (
+    <div className={styles.ModalWrap}>
+      <div className={styles.ModalContent}>
+        <div className={styles.ModalTitle}>당첨된 음식</div>
+        <div className={styles.ModalResultFood}>{props.result}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ResultModal;
