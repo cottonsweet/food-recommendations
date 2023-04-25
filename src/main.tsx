59e65cdc3777290445cee9dbbf9933ad827da308
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-25 14:54:44 +0000

    코드삭제

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  // </React.StrictMode>,
   <BrowserRouter>
     <RecoilRoot>
       <App />
